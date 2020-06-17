7fde524a5e21658a38e3d1dd0297e6c41d0a36a1
marco <marcosigurta.ovb@gmail.com>
2020-06-17 16:46:57 +0200

    Second commit

--- main.js
+++ main.js
@@ -1,45 +1,41 @@
-var name = document.getElementById('name-form').value;
-var traincode = Math.floor(Math.random() * (10000 - 9000 + 1)) + 9000;
-var carriage =  Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-var km = document.getElementById('km').value;
-var categories = document.getElementById('categories').value;
-var price = km * 0.21;
-var discount20 = price * 20 / 100;
-var discount40 = price * 40 / 100;
-var createTicket = document.getElementById('create-ticket');
-var cancelTicket = document.getElementById('cancel-ticket');
+var createTicket = document.getElementById("create-ticket");
+var cancelTicket = document.getElementById("cancel-ticket");
 
-if (categories == "underage") {
-    price = price - discount20;
-} else if (categories == "65+") {
-    price = price - discount40
-} else {
-    price = price;
-}
+createTicket.addEventListener("click", function () {
+    var name = document.getElementById("name-form").value;
+    var km = document.getElementById("km").value;
+    var categories = document.getElementById("categories").value;
+    var price = km * 0.21;
+    var discount20 = (price * 20) / 100;
+    var discount40 = (price * 40) / 100;
 
-createTicket.addEventListener("click" , function(){
-    document.getElementById('ticket').style.opacity = "1";
-    document.getElementById('name').innerHTML = name;
-    document.getElementById('traincode').innerHTML = traincode;
-    document.getElementById('carriageNumber').innerHTML = carriage;
-    document.getElementById('age').innerHTML = categories;
-    document.getElementById('ticketDetails').innerHTML = "Ticket Price: " + price.toFixed(2) + " €"
-})
+    if (categories == "underage") {
+        price = price - discount20;
+    } else if (categories == "65+") {
+        price = price - discount40;
+    } else {
+        price = price;
+    }
 
-cancelTicket.addEventListener("click" , function(){
-    document.getElementById('ticket').style.opacity = "0";
-    document.getElementById('name').innerHTML = "";
-    document.getElementById('traincode').innerHTML = "";
-    document.getElementById('carriageNumber').innerHTML = "";
-    document.getElementById('age').innerHTML = "";
-    document.getElementById('ticketDetails').innerHTML = "Ticket Price: " + price.toFixed(2) + " €"
-})
+    var traincode = Math.floor(Math.random() * (10000 - 9000 + 1)) + 9000;
+    var carriage = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    document.getElementById("ticket").style.opacity = "1";
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("traincode").innerHTML = traincode;
+    document.getElementById("carriageNumber").innerHTML = carriage;
+    document.getElementById("age").innerHTML = categories;
+    document.getElementById("ticketDetails").innerHTML =
+        "Ticket Price: " + price.toFixed(2) + " €";
+});
 
-
-
-
-
-
-
-
-
+cancelTicket.addEventListener("click", function () {
+    document.getElementById("ticket").style.opacity = "0";
+    document.getElementById("name").innerHTML = "";
+    document.getElementById("name-form").value = "";
+    document.getElementById("km").value = "";
+    document.getElementById("traincode").innerHTML = "";
+    document.getElementById("carriageNumber").innerHTML = "";
+    document.getElementById("age").innerHTML = "";
+    document.getElementById("ticketDetails").innerHTML =
+        "Ticket Price: " + price.toFixed(2) + " €";
+});
